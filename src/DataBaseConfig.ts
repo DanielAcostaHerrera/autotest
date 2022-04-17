9b90bf3b37dc7766b2b34c8b469011fb14ff2cca
Rogeidis Sifontes <sifontes.rogeidis@seiza-ti.cl>
2022-04-17 15:19:05 -0400

    addconfig

--- src/DataBaseConfig.ts
+++ src/DataBaseConfig.ts
@@ -1,21 +1,22 @@
 import * as path from 'path';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
+const config = require('../config.json');
 
-const env = process.env.NODE_ENV || 'dev';
-const dotenv_path = path.resolve(process.cwd(), `.${env}.env`);
+//const env = process.env.NODE_ENV || 'dev';
+const dotenv_path = path.resolve(process.cwd(), `.env`);
 const result = dotenv.config({ path: dotenv_path });
 if (result.error) { /* do nothing */ }
 
 export const DatabaseConfig = {
-  type: process.env.TYPEORM_TYPE as any,
-  database: process.env.TYPEORM_DATABASE,
-  port: parseInt(process.env.TYPEORM_PORT),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  host: process.env.TYPEORM_HOST,
+  type: config[process.env.RUN].TYPEORM_TYPE as any,
+  database: config[process.env.RUN].TYPEORM_DATABASE,
+  port: parseInt(config[process.env.RUN].TYPEORM_PORT),
+  username: config[process.env.RUN].TYPEORM_USERNAME,
+  password: config[process.env.RUN].TYPEORM_PASSWORD,
+  host: config[process.env.RUN].TYPEORM_HOST,
   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
   synchronize: false,
 }
-
+console.log(DatabaseConfig)
 export default DatabaseConfig;
