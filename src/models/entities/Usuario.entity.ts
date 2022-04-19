bd3f4abec0857760a815ff9453a38f53c7e4df31
daniel acosta <david.daniel@seiza-ti.cl>
2022-04-18 22:27:13 -0400

    actualizado el modelo usuario

--- src/models/entities/Usuario.entity.ts
+++ src/models/entities/Usuario.entity.ts
@@ -1,10 +1,16 @@
-import { Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { TrabajadorMuestra } from "./TrabajadorMuestra.entity";
 
 @Entity("usuario", { schema: "achs-dev" })
 export class Usuario {
   @PrimaryGeneratedColumn({ type: "int", name: "id_usuario" })
   idUsuario: number;
+
+  @Column("varchar", { name: "email", length: 100 })
+  email: string;
+
+  @Column("varchar", { name: "firebase_uuid", length: 100 })
+  firebaseUuid: string;
 
   @OneToMany(
     () => TrabajadorMuestra,
